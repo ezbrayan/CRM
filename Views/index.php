b0f915be087278fb22d4a98ed1e098d7712a7f72
ezbrayan <150836533+ezbrayan@users.noreply.github.com>
2024-03-02 07:06:24 -0500

    indes usuarios logueados

--- Views/index.php
+++ Views/index.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 include("../Config/validarSesion.php");
 ?>
@@ -9,6 +9,5 @@ Bienvenido usuario
         <span>Salir</span>
     </a>
 </li>
-=======
-holiwis
->>>>>>> 9c132d36d2a0a98b004098edb15558ab9c4f8b7d
+
+hola
